422d24055d254217997818abb7004a79f81f0276
Hanju Kim <hjkim2902@gmail.com>
2023-08-07 14:39:42 +0900

    [김한주] BackTracking Commit

---
+++ week2/BOJ_1759.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+L,C = map(int,input().split())
+li = input().split()
+visited = [False] * C
+
+li.sort()
+
+def dfs(n, res, i):
+    if n == L:
+        cnt = 0
+        for i in res:
+            if i == 'a' or i == 'u' or i == 'i' or i == 'e' or i == 'o':
+                cnt+=1
+        
+        if cnt >= 1 and (len(res) - cnt >= 2):
+            print(res)
+        return
+    
+    for i in range(i,len(li)):
+        if visited[i]:
+            continue
+        visited[i] = True
+        dfs(n+1,res + li[i], i)
+        visited[i] = False
+
+dfs(0,"",0)
