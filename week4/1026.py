769d8eb504d6901a2909c6492f63417411d3bd5b
JieunKim <78676660+mellykim123@users.noreply.github.com>
2023-08-22 17:20:38 +0900

    1026 commit
    다른 풀이 추가

--- week4/1026.py
+++ week4/1026.py
@@ -11,3 +11,17 @@ for i in range(len(arr1)):
     sum+= arr1[i]*arr2[i]
 print(sum)
 
+#다른 풀이
+# n = int(input())
+
+# a_list = list(map(int, input().split()))
+# b_list = list(map(int, input().split()))
+
+# s = 0
+# for i in range(n):
+#     s += min(a_list) * max(b_list)
+#     a_list.pop(a_list.index(min(a_list)))
+#     b_list.pop(b_list.index(max(b_list)))
+
+# print(s)
+
